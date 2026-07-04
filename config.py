41245d62fdf784e306c917fd0a5e618b8ecd498d
rajabanna554-arch <rajabanna554@gmail.com>
2026-07-04 12:54:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,12 +13,12 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
+API_ID = int(getenv("39682304", ""))
+API_HASH = getenv("da5b28e3e90a7d49b930a89f436fc327", "")
+BOT_TOKEN = getenv("8570595039:AAE4nHTU4x0JaPK4HQvj84SK6yME2RXsXkU", "")
+OWNER_ID = list(map(int, getenv("7365576089", "").split()))
+MONGO_DB = getenv("mongodb+srv://valanamrajitsinh9_db_user:1gFCstnZ9Zjie1d7@cluster0.s0sv5ha.mongodb.net/?appName=Cluster0", "")
+LOG_GROUP = getenv("7365576089", "")
 CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
